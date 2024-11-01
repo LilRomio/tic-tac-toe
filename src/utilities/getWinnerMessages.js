19274479bb781465087b0bf8d7893823d55f7eb2
LilRomio <popescu.romeo9@gmail.com>
2024-11-01 11:28:32 +0200

    messages update

--- src/utilities/getWinnerMessages.js
+++ src/utilities/getWinnerMessages.js
@@ -5,13 +5,14 @@ export const getWinnerMessages = (winner, player, draw, currentPlayer) => {
   let messageClass = '';
 
   if (winner) {
-    message = winner === player ? 'You Win!' : 'You Lose!';
+    message =
+      winner === player ? ['You Win!', ' ', 'Game will restart soon'] : ['You Lose!', ' ', 'Game will restart soon'];
     messageClass = winner === player ? 'win' : 'lose';
   } else if (draw) {
-    message = "It's a Draw!";
+    message = ["It's a Draw!", ' ', 'Game will restart soon'];
     messageClass = 'draw';
   } else {
-    message = currentPlayer === player ? 'Your Turn' : "Opponent's Turn";
+    message = currentPlayer === player ? 'Your Turn' : 'Wait your opponent';
     messageClass = 'turn';
   }
   return {
